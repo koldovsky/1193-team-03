efeb636eb1d5181fa233c7712e0deebd1d2c82a0
Nazar1203 <134736778+Nazar1203@users.noreply.github.com>
2024-05-22 15:33:23 +0300

    Update tasks-02.js

---
+++ .github/workflows/js-tasks/tasks-02.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+function stringToArray(string) {
+  return string.split(" ");
+}
+// https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+function DNAtoRNA(dna) {
+  return dna.replace(/T/g, "U");
+  
