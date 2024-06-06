841b670d17ac70d3f972d4573d96e906f62a9a54
Sergij95 <zakharchuksergij@gmail.com>
2024-06-06 06:47:36 +0000

    add clock

---
+++ JS/clock.js
@@ -0,0 +1,11 @@
+function updateClock() {
+    const clockElement = document.getElementById('clock');
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
